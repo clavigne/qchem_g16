0d7fd84e8dda01fd459e201e56986ff6e85bb969
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-03-19 11:18:22 -0400

    parsing works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 // use clap::{crate_version, App, Arg};
 use std::env;
 use std::fs::{read_to_string, File};
-use std::io::{Error, ErrorKind, Result, Write};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Result, Write};
 use std::path::Path;
 
 const CONV_FACTOR: f64 = 4.46552493159e-4;
+
+fn parse_energy(p: &Path) -> Result<f64> {
+    let f = File::open(p)?;
+    let f = BufReader::new(f);
+    let eline = f
+        .lines()
+        .filter_map(|x| match x {
+            Ok(val) => val
+                .starts_with(" The QM part of the energy is")
+                .then(|| val),
+            Err(_) => None,
+        })
+        .next();
+    // now an Option(str)
+    let out = match eline {
+        Some(val) => Ok(val),
+        None => Err(Error::new(
+            ErrorKind::Other,
+            "no energy line found in output file",
+        )),
+    };
+
+    match out {
+        Ok(val) => val
+            .strip_prefix(" The QM part of the energy is")
+            .unwrap()
+            .trim()
+            .parse::<f64>()
+            .map_err(|_| Error::new(ErrorKind::Other, "failed to parse floats")),
+        Err(e) => Err(e),
+    }
+}
 
 fn parse_floats_from_file(n: u8, p: &Path) -> Result<Vec<f64>> {
     // Parse a vector of floats from a file.
@@ -25,11 +57,12 @@ fn parse_floats_from_file(n: u8, p: &Path) -> Result<Vec<f64>> {
     }
 }
 
-fn write_gaussian_output(natoms: u8, nder: u8, qchem_loc: &str, output_file: &str) -> Result<()> {
+fn translate_to_gaussian(natoms: u8, nder: u8, qchem_loc: &str, output_file: &str) -> Result<()> {
     let mut outfile = File::create(output_file)?;
 
     // energy
-    outfile.write(format!("{:+20.12}", 0.0).as_bytes())?;
+    let energy = parse_energy(&Path::new(&qchem_loc).join("qchem.out"))?;
+    outfile.write(format!("{:+20.12}", energy).as_bytes())?;
 
     // dipole
     outfile.write(format!("{:+20.12}{:+20.12}{:+20.12}\n", 0.0, 0.0, 0.0).as_bytes())?;
@@ -65,16 +98,10 @@ fn write_gaussian_output(natoms: u8, nder: u8, qchem_loc: &str, output_file: &st
     Ok(())
 }
 
-fn main() {
-    let nder = 2;
-    let qchem_loc = env::var("QCHEM_RUNDIR").unwrap_or(".".to_string());
-    let output = "gamout";
-
-    write_gaussian_output(3, nder, &qchem_loc, output);
-
+fn main() -> Result<()> {
     // let matches = App::new("qchem-gaussian-collab")
     //     .author("Cyrille Lavigne <cyrille.lavigne@mail.utoronto.ca>")
-    //     .about("bla")
+    //     .about("TODO")
     //     .version(crate_version!())
     //     .arg(
     //         Arg::new("nel")
@@ -127,4 +154,10 @@ fn main() {
     //     Ok(i) => i,
     //     Err(e) => e.exit(),
     // };
+
+    let qchem_loc = env::var("QCHEM_RUNDIR").unwrap_or(".".to_string());
+    let nder = 2;
+    let output = "gamout";
+    translate_to_gaussian(3, nder, &qchem_loc, output)?;
+    Ok(())
 }
